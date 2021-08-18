5a367b7b8d62af06406fb701801e34263067e64f
andyexeter <palmer.andy@gmail.com>
2021-08-18 08:56:28 +0100

    Update to php-cs-fixer 3. Update rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 use Palmtree\PhpCsFixerConfig\Config;
 
@@ -7,6 +9,7 @@ $config = new Config();
 $config
     ->getFinder()
     ->in(__DIR__ . '/src')
-    ->append([__FILE__]);
+    ->append([__FILE__])
+;
 
 return $config;
--- src/Config.php
+++ src/Config.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace Palmtree\PhpCsFixerConfig;
 
@@ -9,21 +11,29 @@ final class Config extends \PhpCsFixer\Config
         parent::__construct('Palmtree');
 
         $this->setRules([
-            '@Symfony'                      => true,
-            '@Symfony:risky'                => true,
-            'array_syntax'                  => ['syntax' => 'short'],
-            'concat_space'                  => ['spacing' => 'one'],
-            'fopen_flags'                   => false,
-            'ordered_imports'               => true,
-            'protected_to_private'          => false,
-            'binary_operator_spaces'        => ['align_double_arrow' => true, 'align_equals' => true],
-            'yoda_style'                    => false,
-            'phpdoc_inline_tag'             => false,
+            '@Symfony' => true,
+            '@Symfony:risky' => true,
+            'declare_strict_types' => true,
+            // Prevent Psalm inline annotations being stripped. https://github.com/FriendsOfPHP/PHP-CS-Fixer/issues/4446
+            'phpdoc_to_comment' => false,
+            'concat_space' => ['spacing' => 'one'],
+            'fopen_flags' => false,
+            'ordered_imports' => true,
+            'protected_to_private' => false,
+            'binary_operator_spaces' => [
+                'operators' => [
+                    // Prevent spaces being added in PHP8 union types
+                    '|' => null,
+                ],
+            ],
+            // Bad yoda style is
+            'yoda_style' => false,
             'phpdoc_annotation_without_dot' => false,
-            'cast_spaces'                   => ['space' => 'none'],
-            'no_superfluous_phpdoc_tags'    => true,
-            'declare_strict_types'          => true,
-            'echo_tag_syntax'               => false,
+            'cast_spaces' => ['space' => 'none'],
+            'no_superfluous_phpdoc_tags' => [
+                'allow_mixed' => true,
+            ],
+            'echo_tag_syntax' => false,
         ]);
 
         $this->setRiskyAllowed(true);
